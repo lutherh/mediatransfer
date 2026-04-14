71ae6be1a9815c30d94546045d5a8e25ca8ed4ad
lutherh <izbrannick@gmail.com>
2026-04-14 19:21:18 +0200

    feat: start-up script

--- scripts/start-all.cmd
+++ scripts/start-all.cmd
@@ -1,2 +1,20 @@
 @echo off
-"C:\Program Files\Git\bin\bash.exe" -c "cd /c/dev/PhotosBackup/mediatransfer && ./scripts/start-all.sh"
+setlocal
+
+REM Resolve mediatransfer root (parent of scripts/)
+for %%I in ("%~dp0..") do set "ROOT_DIR=%%~fI"
+
+REM Find Git Bash
+if exist "C:\Program Files\Git\bin\bash.exe" (
+    set "GIT_BASH=C:\Program Files\Git\bin\bash.exe"
+    goto :found
+)
+if exist "C:\Program Files (x86)\Git\bin\bash.exe" (
+    set "GIT_BASH=C:\Program Files (x86)\Git\bin\bash.exe"
+    goto :found
+)
+echo ERROR: Git Bash not found
+exit /b 1
+
+:found
+"%GIT_BASH%" -c "cd \"$(cygpath '%ROOT_DIR%')\" && ./scripts/start-all.sh %*"
