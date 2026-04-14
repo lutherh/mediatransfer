9dd806f85c5d7ac4d4907ccafc39905a3f580e41
lutherh <izbrannick@gmail.com>
2026-04-14 19:21:18 +0200

    feat: start-up script

---
+++ scripts/register-startup.cmd
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+
+REM Register or unregister MediaTransfer to start on Windows logon.
+REM Usage:
+REM   register-startup.cmd           - register the scheduled task
+REM   register-startup.cmd remove    - remove the scheduled task
+
+for %%I in ("%~dp0..") do set "ROOT_DIR=%%~fI"
+set "TASK_NAME=MediaTransfer-StartAll"
+set "START_CMD=%ROOT_DIR%\scripts\start-all.cmd"
+
+if /i "%~1"=="remove" goto :remove
+
+echo Registering "%TASK_NAME%" to run at logon...
+schtasks /create /tn "%TASK_NAME%" /tr "\"%START_CMD%\"" /sc onlogon /rl highest /f
+if %errorlevel% equ 0 (
+    echo Done. MediaTransfer will start automatically on logon.
+) else (
+    echo ERROR: Failed to create scheduled task. Try running as Administrator.
+    exit /b 1
+)
+goto :eof
+
+:remove
+echo Removing "%TASK_NAME%" scheduled task...
+schtasks /delete /tn "%TASK_NAME%" /f
+if %errorlevel% equ 0 (
+    echo Done. MediaTransfer will no longer start on logon.
+) else (
+    echo ERROR: Failed to remove scheduled task.
+    exit /b 1
+)
